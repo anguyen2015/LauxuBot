42924c2256dc6b2ef1920ab6cd708f422420fb41
Andrew Nguyen <anguyen2015@my.fit.edu>
2018-04-10 13:48:46 -0400

    Initial commit

---
+++ lauxu_bot.py
@@ -0,0 +1,24 @@
+import discord
+
+TOKEN = 'NDMzMjczMDM1MTE4NjA4Mzg0.Da5x0Q.nvRW5DWnGsu32z1N9Cwf3MLccfI'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
